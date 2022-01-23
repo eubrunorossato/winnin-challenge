7135f34ab3602d5a715d58a3961541aee7b8b37f
eubrunorossato <Bruno.Araujo1@ibm.com>
2022-01-23 17:19:48 -0300

    set https server

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 import app from './src/config/express';
-app.listen(3000, () => {
-  console.log('app is listening to port 3000');
-});
+import https from 'https';
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const cb = () => console.log(`Server Running on Port ${process.env.PORT}`);
+
+if (process.env.NODE_ENV === 'LOCALHOST') {
+  https
+    .createServer(
+      {
+        key: fs.readFileSync('./certificates/key.pem'),
+        cert: fs.readFileSync('./certificates/cert.pem'),
+      },
+      app
+    )
+    .listen(process.env.PORT, cb);
+} else {
+  app.listen(process.env.PORT, cb);
+}
