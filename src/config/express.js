2a4c04e07cadda116594256e95a3b905d4c65bd6
eubrunorossato <Bruno.Araujo1@ibm.com>
2022-01-24 20:35:54 -0300

    add helmet to api

--- src/config/express.js
+++ src/config/express.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import routes from '../router';
+import helmet from 'helmet'
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(routes);
+app.use(helmet())
 
 export default app;
